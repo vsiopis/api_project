11cd455840a7256824473cc586aa3951504a0c84
vsiopis <vsiopi@gmail.com>
2017-02-07 14:42:36 +0200

    Create edit update destroy action and change show template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -17,10 +18,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      render 'show'
+    else
+      render 'edit'
+    end 
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:post_text) 
