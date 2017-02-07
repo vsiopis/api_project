02eafac3a6e0766913827c71f1066cd627a70187
vsiopis <vsiopi@gmail.com>
2017-02-07 15:06:18 +0200

    Create index show new create edit destroy actions for categories and index show edit templates

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,44 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render 'new'
+
+    end 
+  end
+
+  def show
+  end
+    
+  def edit
+  end
+
+  def update
+    if @category.update(category_params) 
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
+  end
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
