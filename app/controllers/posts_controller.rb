78372164d8ea48fbce829cf6a447a1bc5bbe3695
vsiopis <vsiopi@gmail.com>
2017-02-07 14:20:11 +0200

    Create index new show action and templates

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_text) 
+  end
+end
