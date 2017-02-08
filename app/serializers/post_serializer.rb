08bd11ccea11107e3ef7c663a5a7a211bbaa3755
vsiopis <vsiopi@gmail.com>
2017-02-08 13:09:07 +0200

    Add created_at column in serializer post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :post_text
+  attributes :id, :post_text, :created_at
 
   has_many :post_categories
   has_many :categories, through: :post_categories
